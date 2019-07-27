35448d3b635367565f5305b8167a343809f9dfb6
Ralph Sablaon <rsablaon88@gmail.com>
2019-07-26 22:14:49 -0700

    HTML done, JS done (need to double check), Need to finalize CSS

---
+++ assets/javascript/app.js
@@ -0,0 +1,103 @@
+// JS file for HOMEWORK #5 
+
+$(document).ready(function() {
+    // when the page is loaded first, the timer, questions and summary must be hidden.
+    
+        $('#countdown').hide();
+        $('.trivia-ques').hide();
+        $('.results').hide();
+        $('#playagain').hide();
+    
+        // Declare Variables
+        var number = 60; //number of seconds
+        var intervalId;
+        var correctCount = 0;
+        var wrongCount = 0;
+        var unanswered = 0;
+    
+    //ALL FUNCTIONS
+    
+        // function to show questions
+        function showQuestions(){
+            $('#countdown').show();
+            $('.trivia-ques').show();
+            $('#game-done').show();
+            $('#playagain').hide();
+        }
+    
+        // function for timer
+        function countdownTimer(){
+                intervalId = setInterval(decrement, 1000);
+        }
+       
+            // function to decrement timer
+        function decrement(){
+            number--;
+            $('#timer').html(" " + number + " " + "seconds");
+            if (number === 1){
+                $('#timer').html(" " + number + " " + "second");
+            }
+            else if(number === 0) {
+                stop();
+                hide();
+                displaySummary();
+            }
+        }
+    
+            //function to clear timer
+        function stop() {
+            clearInterval(intervalId);
+        }
+    
+            //function to hide text after questions are answered or timer out
+        function hide(){
+            $('#countdown').hide();
+            $('.trivia-ques').hide();
+            $('#game-done').hide();
+        }
+    
+        // function to display summary of game
+        function displaySummary(){
+            $('.results').show();
+            unanswered = (6-(correctCount+wrongCount));
+            $('#correctScore').text("Correct Answers:" + " " + correctCount); 
+            $('#wrongScore').text("Wrong Answers:" + " " + wrongCount); 
+            $('#unanswered').text("Unanswered:" + " " + unanswered);
+            $('#playagain').show(); 
+        }
+    
+    // CLICK EVENTS
+    
+        //Clicking Start Button
+        $('#game-start').on('click', function(){
+            $('#game-start').hide();
+            showQuestions();
+            countdownTimer();
+        }); 
+    
+        //Clicking Done Button
+        $('#game-done').on('click', function(){
+            $('#game-start').hide(); 
+            hide();
+            displaySummary();
+        });
+        
+        // Clicking Play Again Button
+        $('#playagain').on('click', function() {
+            $('.results').hide();
+            $('#game-start').show();
+            $('#playagain').hide();
+            
+            
+        });
+
+    
+        //Clicking Radio button
+        $('input[type=radio]').on ('change', function(){
+        correctCount = $('input[value=correct]:checked').length;
+        wrongCount = $('input[value=wrong]:checked').length;
+        unanswered = (6-(correctCount+wrongCount));
+        });
+    
+   
+    });
